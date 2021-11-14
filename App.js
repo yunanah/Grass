7b0dccac70bd983808448735f0fd5424a033ab84
yunanah <bbyl6319@gmail.com>
2021-11-15 03:15:18 +0900

    🎨 Set Model and status funcs : Todo Input Modal

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { createElement } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Touchable } from 'react-native'
+import React, { useState, useLayoutEffect, createElement, useEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Switch, TextInput } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { FontAwesome, FontAwesome5, AntDesign, Ionicons } from '@expo/vector-icons'
+import Todo from './Todo'
 
 const Stack = createNativeStackNavigator()
 const BottomTabs = createBottomTabNavigator()
@@ -93,48 +94,48 @@ const Tabs = (props) => {
           headerTitleStyle: {
               fontWeight: 'bold',
           },
-          headerRight: () => (
-            <TouchableOpacity 
-                style={{ marginRight: 15, color: 'ivory' }}
-                onPress={() => console.log('info')}
-            >
-                <CustomIcon
-                    type='Ionicons'
-                    icon='information-circle-outline'
-                    color="ivory"
-                    size={25}
-                />
-            </TouchableOpacity>
-        )
+        //   headerRight: () => (
+        //     <TouchableOpacity 
+        //         style={{ marginRight: 15, color: 'ivory' }}
+        //         onPress={() => console.log('info')}
+        //     >
+        //         <CustomIcon
+        //             type='Ionicons'
+        //             icon='information-circle-outline'
+        //             color="ivory"
+        //             size={25}
+        //         />
+        //     </TouchableOpacity>
+        // )
         }}
       />
       <BottomTabs.Screen 
         name="Calendar" 
         component={Calendar} 
         options={{
-          headerShown: true, 
-          title: '오늘', 
+          headerShown: false, 
+          // title: '2021', 
           tabBarLabel: '오늘',
-          headerStyle: {
-              backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-              fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <TouchableOpacity 
-                style={{ marginRight: 15, color: 'ivory' }}
-                onPress={() => navigation.push('TodoInput')}
-            >
-                <CustomIcon
-                    type='FontAwesome5'
-                    icon='plus'
-                    color="ivory"
-                    size={25}
-                />
-            </TouchableOpacity>
-        )
+          // headerStyle: {
+          //     backgroundColor: '#f4511e',
+          // },
+          // headerTintColor: '#fff',
+          // headerTitleStyle: {
+          //     fontWeight: 'bold',
+          // },
+        //   headerRight: () => (
+            // <TouchableOpacity 
+            //     style={{ marginRight: 15, color: 'ivory' }}
+            //     onPress={() => navigation.push('TodoInput')}
+            // >
+            //     <CustomIcon
+            //         type='FontAwesome5'
+            //         icon='plus'
+            //         color="ivory"
+            //         size={25}
+            //     />
+            // </TouchableOpacity>
+        // )
         }}
       />
       <BottomTabs.Screen 
@@ -189,14 +190,59 @@ const Calendar = (props) => {
 
   const { navigation, route } = props
 
+  // const [todos, setTodos] = useState([])
+
+  console.log("navigation:" ,navigation)
+
   return (
     <Stack.Navigator>
       <Stack.Group>
-        <Stack.Screen name="Year" component={Year} options={{ headerShown: false }}/>
-        <Stack.Screen name="Day" component={Day} options={{ headerShown: false }}/>
+        <Stack.Screen 
+          name="Year" 
+          component={Year} 
+          options={{ 
+            headerShown: true,
+            title: '2021', 
+            // tabBarLabel: '2021',
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            }
+          }}
+          // initialParams={{
+          //   todos: todos,
+          //   setTodos: setTodos
+          // }}
+          
+        />
+        <Stack.Screen 
+          name="Day" 
+          component={Day} 
+          options={{ 
+            headerShown: true,
+            title: '오늘', 
+            tabBarLabel: '오늘',
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+          }}
+        />
       </Stack.Group>
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name='TodoInput'    component={TodoInput}   options={{ headerShown: false }}/>
+      <Stack.Group 
+        screenOptions={{ presentation: 'modal' }}
+      >
+        <Stack.Screen 
+          name='TodoInput'    
+          component={TodoInput}   
+          options={{ headerShown: false }}
+        />
       </Stack.Group>
     </Stack.Navigator>
   )
@@ -205,10 +251,69 @@ const Calendar = (props) => {
 const Year = (props) => {
 
   const { navigation, route } = props
+  // const { todos, setTodos } = route.params
+
+  const [todos, setTodos] = useState([])
+
+  console.log('params:',route.params)
+
+  // console.log('todos:', todos)
+  // console.log('setTodos: ',setTodos)
+
+  useEffect(() => {
+    console.log('useEffect')
+    
+    if (route.params?.todo) {
+      // console.log('기존의 할일: ', todos)
+      console.log('ok ok ok')
+
+      const { todo } = route.params
+
+      console.log('todo', todo)
+      const newTodos = [...todos, todo]
+      console.log('newTodos', newTodos)
+
+      setTodos(newTodos)
+      console.log('todos:', todos)
+    }
+
+  }, [route.params])
+
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect')
+    navigation.setOptions({
+      headerRight: () => (
+          <TouchableOpacity 
+            style={{ marginRight: 15, color: 'ivory' }}
+            onPress={() => navigation.push('TodoInput', {
+              name: 'yoora'
+            })}
+          >
+            <CustomIcon
+              type='FontAwesome5'
+              icon='plus'
+              color="ivory"
+              size={25}
+            />
+          </TouchableOpacity>
+      )
+    })
+  }, [navigation])
 
   return (
     <View style={styles.container}>
       <Text>2021 달력입니다</Text>
+
+      {todos.length !== 0 ?
+        todos.map(todo =>
+          <Todo
+            title={todo.title}
+            isKeywork={todo.isKeywork}
+          />
+        ) :
+          <Text>등록된 일정이 없습니다.</Text>
+      }
+
       <TouchableOpacity onPress={() => {
         navigation.push('Day', {
           date: '2021-11-12',
@@ -226,6 +331,26 @@ const Day = (props) => {
 
   const { navigation, route } = props
   const { date, day } = route.params
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+
+
+        <TouchableOpacity 
+          style={{ marginRight: 15, color: 'ivory' }}
+          onPress={() => navigation.push('TodoInput')}
+        >
+          <CustomIcon
+            type='FontAwesome5'
+            icon='plus'
+            color="ivory"
+            size={25}
+          />
+        </TouchableOpacity>
+      )
+    })
+  }, [navigation])
 
   return (
     <View style={styles.container}>
@@ -251,15 +376,51 @@ const Day = (props) => {
 const TodoInput = (props) => {
 
   const { navigation, route } = props
+  const [isKeywork, setIsKeywork] = useState(false)
+  const toggleSwitch = () => setIsKeywork(previousState => !previousState)
+  const [todoTitle, setTodoTitle] = useState('')
 
   return (
-    <View>
-      <Text>할일 입력하는 모달입니다</Text>
-      <TouchableOpacity onPress={() => {
-        navigation.goBack()
-      }}
+    <View style={styles.container}>
+      <TextInput 
+        style={styles.input}
+        onChangeText={title => {
+          console.log('title:', title)
+          setTodoTitle(title)
+        }}
+        placeholder="무엇을 하실 계획인가요?"
+        value={todoTitle}
+      />
+
+      <Text>오늘의 중요한 일인가요?</Text>
+      <Switch
+        trackColor={{ false: 'floralwhite', true: 'darkseagreen' }}
+        thumbColor={isKeywork ? 'gloralwhite' : 'darkgreen'}
+        ios_backgroundColor='yellowgreen'
+        onValueChange={toggleSwitch}
+        value={isKeywork}
+      />
+      <TouchableOpacity 
+        onPress={() => {
+
+          navigation.navigate('Year', {
+            todo: {
+              title: todoTitle,
+              isKeywork: isKeywork,
+            }
+          })
+
+          // navigation.push('Year', {
+          //   todo: {
+          //     title: todoTitle,
+          //     isKeywork: isKeywork,
+          //   }
+          // })
+        }}
       >
-        <Text>닫기</Text>
+        <View style={styles.button_add}>
+          <Text style={styles.button_add_title}>등록하기</Text>
+        </View>
       </TouchableOpacity>
     </View>
   )
@@ -284,10 +445,28 @@ const Rank = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    // borderWidth: 1,
+    borderBottomWidth: 1,
+    padding: 10
+  },  
+  button_add: {
+    // color: 'white',
+    backgroundColor: 'darkolivegreen',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 4,
+    margin: 12
+  },
+  button_add_title: {
+    color: 'white'
+  }
 })
 
 export default App
